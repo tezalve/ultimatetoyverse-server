2f99d896d6932f11f54021c962df39f0b3725b2e
tezalve <tezalve@gmail.com>
2023-05-21 20:24:04 +0600

    :bug: fixing cors policy

--- index.js
+++ index.js
@@ -5,14 +5,8 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-// app.use(cors());
-const corsConfig = {
-    origin: '',
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE']
-}
-app.use(cors(corsConfig))
-app.options("", cors(corsConfig))
+app.use(cors())
+
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.vvdmedc.mongodb.net/?retryWrites=true&w=majority`;
